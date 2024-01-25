7a10d8f252aa5827c6d1734155157a63101a16c6
starglad <rjmmarcelo@hotmail.com>
2024-01-25 19:40:22 -0300

    feat: create search component

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,12 +3,15 @@ import gitLogo from '../assets/github.png'
 import {Container} from "./styles";
 import Input from "../components/Input";
 import ItemRepo from "../components/ItemRepo";
+import Button from "../components/Button";
 
 function App() {
+
     return (
         <Container>
             <img src={gitLogo} width={72} height={72} alt={'Github Logo'}/>
             <Input/>
+            <Button/>
             <ItemRepo/>
         </Container>
     );
